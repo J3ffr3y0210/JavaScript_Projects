9f0916fd2b87c8fcc1e560aedddfc99c0ba33dcb
J3ffr3y0210 <jerhine16@gmail.com>
2022-02-20 19:02:59 -0800

    submit-Project2_functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
+
+function MyFunction(){          //learning to Concatenate
+    var sentence="I am learning";
+    sentence += "A lot from this course!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
